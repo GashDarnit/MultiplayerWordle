28371a12ce831200f255628ef2d1af1cc902cbae
GashDarnit <edisontanh@gmail.com>
2023-11-28 12:24:40 +0800

    Test fetching solution from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Game from './Game.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Game from './Game.tsx';
 
 function App() {
+  const [answer, setAnswer] = useState('');
   return (
     <div className="App">
       <header className="App-header">
         <h1>Multiplayer Wordle</h1>
       </header>
+      
       <div className="d-flex flex-column align-items-center">
-        <button>Singleplayer</button>
+        <button onClick = {() => { console.log(fetchSolution()) }}>Singleplayer</button>
         
         <button onClick = {() => {
             let socket = new WebSocket('ws://localhost:3000');
@@ -26,5 +30,13 @@ function App() {
   );
 }
 
-
+async function fetchSolution() {
+  try {
+    const response = await fetch('https://random-word-api.herokuapp.com/word?length=5');
+    const data = await response.json();
+    console.log(data[0]);
+  } catch (error) {
+    console.error('Error fetching solution:', error);
+  }
+}
 export default App;
