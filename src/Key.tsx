b25af72b0dd41411772111490cebb8b2dbeb0f89
GashDarnit <edisontanh@gmail.com>
2023-11-30 17:18:26 +0800

    Added keyboard layout

---
+++ src/Key.tsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import './App.css';
+
+function Key({ keyVal }) {
+
+  return (
+    <div className = "key"> {keyVal} </div>
+  );
+}
+
+export default Key;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Keyboard from './Keyboard.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App /> 
+    <Keyboard />
   </React.StrictMode>,
 )
