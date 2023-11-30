6d600137b2dc57794e4e87158a2b9aed6f221af8
GashDarnit <edisontanh@gmail.com>
2023-11-30 22:48:19 +0800

    Added function to create boards with custom length

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,60 @@
-import { useState } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import './App.css';
 import Game from './Game.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { boardDefault, generateWordSet } from "./Words";
+
 var HOST = '127.0.0.1';
 var PORT = 3000;
 
+export const AppContext = createContext();
+
 function App() {
   const [answer, setAnswer] = useState('');
+  const [wordLength, setWordLength] = useState(5);
+  const [board, setBoard] = useState(generateBoard(5));
+  
+
+    const handleWordLength = () => {
+        setWordLength(7);
+    }
+
+    const handleNewBoard = () => {
+        let newArray = generateBoard(wordLength);
+        setBoard(newArray);
+    }
+    
+    
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Multiplayer Wordle</h1>
-      </header>
+    <div className="App">    
+        <header className="App-header">
+            <h1>Multiplayer Wordle</h1>
+        </header>
       
-      <div className="d-flex flex-column align-items-center">
-        <button onClick = {() => { console.log(fetchSolution()) }}>Singleplayer</button>
+        <AppContext.Provider 
+            value = {{
+                answer,
+                wordLength,
+                setWordLength,
+                board,
+                setBoard,
+            }}
+        >
+            
+            <div className="d-flex flex-column align-items-center">
+                <button onClick = {() => { console.log(fetchSolution()) }}>Singleplayer</button>
+
+                <button onClick = {handleWordLength}>Multiplayer {wordLength}</button>
+                
+                <button onClick = {handleNewBoard}>Test {board[0][0]}</button>
+                
+            </div>
+
+            <Game length ='3'/>
         
-        <button>Multiplayer</button>
-      </div>
-
-      <Game length={2}/>
-
-
+        </AppContext.Provider>
     </div>
   );
 }
@@ -37,7 +69,19 @@ async function fetchSolution() {
     console.error('Error fetching solution:', error);
   }
 }
-  
-  
+
+function generateBoard(length) {
+    let newArray = [];
+
+    for (let i = 0; i < 6; i++) {
+        const row = [];
+        for (let j = 0; j < length; j++) {
+            row.push("");
+        }
+        newArray.push(row);
+    }
+    return newArray;
+}
+
   
 export default App;
